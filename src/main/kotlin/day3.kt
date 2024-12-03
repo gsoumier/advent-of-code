af806f077e899e2858c4da5791a571288f4c1464
Germain Soumier <germain_soumier@ext.connect-tech.sncf>
2024-12-03 10:20:26 +0100

    [2024] Day 3

---
+++ src/main/kotlin/day3.kt
@@ -0,0 +1,30 @@
+class Day3(inputType: InputType = InputType.FINAL) : AocRunner<String, Long>(
+    3,
+    StringLineParser,
+    inputType
+) {
+    private val pattern = """mul\((\d+),(\d+)\)""".toRegex()
+
+    override fun partOne(): Long {
+        return lines.mul()
+    }
+
+    override fun partTwo(): Long {
+//        return lines.flatMap { extractDo(it) }.mul()
+        return extractDo(lines.joinToString("")).mul()
+    }
+
+    private fun List<String>.mul() =
+        flatMap { pattern.findAll(it) }.sumOf { it.groupValues[1].toLong() * it.groupValues[2].toLong() }
+
+}
+
+fun extractDo(instr: String): List<String> {
+    val split = instr.split("don't()")
+    return listOf(split.first()) + split.drop(1).flatMap { it.split("do()").takeIf { dontPart -> dontPart.size > 1 }?.drop(1) ?: emptyList() }
+}
+
+
+fun main() {
+    Day3().run()
+}
---
+++ src/test/kotlin/Day3Test.kt
@@ -0,0 +1,24 @@
+import io.kotest.matchers.shouldBe
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.Test
+
+class Day3Test {
+
+    @Nested
+    inner class Day3RunnerTest {
+        private val runner = Day3(InputType.SAMPLE)
+
+        @Test
+        fun partOne() {
+            runner.partOne() shouldBe 161
+        }
+
+        @Test
+        fun partTwo() {
+            runner.partTwo() shouldBe 48
+        }
+
+
+    }
+
+}
